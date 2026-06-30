0dce9f8b8cfca59fa24939406d7e043508d66d22
ihsanbaik72316-oss <ihsan.baik72316@gmail.com>
2026-06-30 19:37:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ window.addEventListener('DOMContentLoaded', () => {
 document.getElementById('btn-login').addEventListener('click', () => {
     const pw = passwordInput.value.trim();
     // Kamu bisa ganti password-nya di sini sesukamu
-    if (pw.toLowerCase() === 'ondel ondel') {
+    if (pw.toLowerCase() === 'boneka mampang') {
         music.play().catch(e => console.log("Audio play deferred"));
         switchScreen('screen-login', 'screen-gate');
         
